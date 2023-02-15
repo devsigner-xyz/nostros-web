2dc5042d519b382b7c50e1bb472356fdd2f33376
Pablo Carballeda <pcarballeda@gmail.com>
2023-02-15 03:39:36 +0100

    Configure webpack and add material symbols

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-require('./styles.scss');
+import './styles.scss'
+import ClipboardJS from "clipboard";
+import 'material-symbols/outlined.scss';
+
+const clipboard = new ClipboardJS('.copy');
+clipboard.on('success', (e) => {
+    console.info('Text:', e.text);
+    e.clearSelection();
+});
+
+document.querySelectorAll(".notification .delete")
+    .forEach(($deleteButton) => {
+
+        // Step 2: Get the parent notification
+        // of the delete button
+        const parentNotification = $deleteButton.parentNode;
+
+        // Add click event listener on delete
+        // button and when the button get clicked
+        // remove the parent notification
+        $deleteButton.addEventListener('click', () => {
+            parentNotification.parentNode
+                .removeChild(parentNotification);
+        });
+    });
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,84 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    // Modo de Webpack
+    mode: 'development',
+
+    // Archivo de entrada principal
     entry: './src/index.js',
+
+    // Archivo de salida
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/'
     },
+
+    // Módulos y reglas para compilar y cargar archivos
     module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader'
-                },
-                {
-                    loader: 'sass-loader',
+        rules: [
+            // Regla para compilar archivos SCSS
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    ['autoprefixer'],
+                                ],
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+
+            // Regla para compilar archivos JavaScript ES6
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
                     options: {
-                        sourceMap: true,
-                        // options...
-                    }
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]',
+                },
+            },
+
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]'
                 }
-            ]
-        }]
+            }
+
+        ],
     },
+
+    // Plugins para generar archivos HTML y live reload
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/styles.css'
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: 'index.html',
         }),
-    ]
+    ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
 };
